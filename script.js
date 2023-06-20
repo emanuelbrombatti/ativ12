447a83b54809bcefb0878abc0b49aff5a7e9aabe
emanuelbrombatti <emanuelbds00@gmail.com>
2023-06-19 22:45:29 -0300

    mobile

--- script.js
+++ script.js
@@ -27,4 +27,31 @@ var slider = tns({
     }
   });
   
+  document.addEventListener('DOMContentLoaded', function() {
+    var button = document.querySelector('.hamburger');
+    var body = document.querySelector('body');
+    var linkButtons = document.querySelectorAll('.link-btn');
+  
+    button.addEventListener('click', function() {
+      body.classList.toggle('menu-open');
+    });
+  
+    linkButtons.forEach(function(linkButton) {
+      linkButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        body.classList.remove('menu-open');
+  
+        var targetId = this.getAttribute('href').substring(1);
+        var targetSection = document.getElementById(targetId);
+        if (targetSection) {
+          targetSection.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+    });
+  });
+  
+  
+  
   
